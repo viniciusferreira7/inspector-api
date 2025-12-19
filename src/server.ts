d52b98958eca4dfbc496eb804817a44ac8dd6134
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-12-19 08:43:40 -0300

    chore: add swagger for documentation

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,7 @@ import { fastify } from 'fastify';
 const app = fastify();
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-	console.log('HTTP server running! 🚀');
+  console.log('HTTP server running! 🚀');
 });
+
+//TODO: 18:00
