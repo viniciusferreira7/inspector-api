06548fc8f385820991feb129a58dc679e732e704
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-02-21 15:04:28 -0300

    feat: create route to delete webhook by id

---
+++ src/http/routes/delete-webhook-by-id.ts
@@ -0,0 +1,37 @@
+import { eq } from 'drizzle-orm';
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
+import z from 'zod';
+import { drizzleDb } from '@/db';
+import { webhooks } from '@/db/schema';
+
+export const deleteWebhookById: FastifyPluginAsyncZod = async (app) => {
+  app.delete(
+    '/api/webhooks/:id',
+    {
+      schema: {
+        summary: 'delete webhook by ID',
+        operationId: 'deleteWebhookById',
+        tags: ['Webhooks'],
+        params: z.object({
+          id: z.uuidv7(),
+        }),
+        response: {
+          204: z.void(),
+          404: z.object({ message: z.string() }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const result = await drizzleDb
+        .delete(webhooks)
+        .where(eq(webhooks.id, request.id))
+        .returning();
+
+      if (!result.length) {
+        return reply.status(404).send({ message: 'Webhook not found' });
+      }
+
+      return reply.status(204).send();
+    }
+  );
+};
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { env } from './env';
 import { fastifyCors } from './http/plugins/fastify-cors';
 import { fastifySwagger } from './http/plugins/fastify-swagger';
 import { zodTypeProvider } from './http/plugins/zod-type-provider';
+import { deleteWebhookById } from './http/routes/delete-webhook-by-id';
 import { getWebhookById } from './http/routes/get-webhook-by-id';
 import { listWebhooks } from './http/routes/list-webhooks';
 
@@ -14,6 +15,7 @@ fastifySwagger(app);
 
 app.register(listWebhooks);
 app.register(getWebhookById);
+app.register(deleteWebhookById);
 
 app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log(`HTTP server running on port ${env.PORT}! 🚀`);
