caf6743fad9f1ebb89d238a53a1444b90df9fdc9
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-02-21 15:25:16 -0300

    feat: finish route to list webhooks

--- src/http/routes/list-webhooks.ts
+++ src/http/routes/list-webhooks.ts
@@ -1,5 +1,9 @@
+import { desc, lt } from 'drizzle-orm';
+import { createSelectSchema } from 'drizzle-zod';
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 import z from 'zod';
+import { drizzleDb } from '@/db';
+import { webhooks } from '@/db/schema';
 
 export const listWebhooks: FastifyPluginAsyncZod = async (app) => {
   app.get(
@@ -11,19 +15,50 @@ export const listWebhooks: FastifyPluginAsyncZod = async (app) => {
         tags: ['Webhooks'],
         querystring: z.object({
           limit: z.coerce.number().min(1).max(100).default(20),
+          cursor: z.uuidv7().optional(),
         }),
         response: {
-          200: z.array(
-            z.object({
-              id: z.string(),
-              method: z.string(),
-            })
-          ),
+          200: z.object({
+            webhooks: z.array(
+              createSelectSchema(webhooks).pick({
+                id: true,
+                method: true,
+                pathname: true,
+                createdAt: true,
+              })
+            ),
+            nextCursor: z.uuidv7().nullable(),
+          }),
         },
       },
     },
     async (request, reply) => {
-      return reply.status(200).send([{ id: '123', method: 'POST' }]);
+      const result = await drizzleDb
+        .select({
+          id: webhooks.id,
+          method: webhooks.method,
+          pathname: webhooks.pathname,
+          createdAt: webhooks.createdAt,
+        })
+        .from(webhooks)
+        .where(
+          request.query.cursor
+            ? lt(webhooks.id, request?.query?.cursor)
+            : undefined
+        )
+        .orderBy(desc(webhooks.id))
+        .limit(request.query.limit + 1);
+
+      const hasMore = result.length > request.query.limit;
+
+      const items = hasMore ? result.splice(0, request.query.limit) : result;
+
+      const lastWebhook = result[result.length - 1];
+
+      reply.status(200).send({
+        webhooks: items,
+        nextCursor: hasMore ? lastWebhook?.id : null,
+      });
     }
   );
 };
